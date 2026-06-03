16da28c089eed297e8f63c8536290ad5f4cc5384
Alex Carney <alcarneyme@gmail.com>
2026-06-03 23:46:45 +0100

    Start thinking about a reproduceable environment

---
+++ .devcontainer/tools.mk
@@ -0,0 +1,32 @@
+ARCH ?= $(shell arch)
+BIN ?= $(HOME)/.local/bin
+
+ifeq ($(strip $(ARCH)),)
+$(error Unable to determine platform architecture)
+endif
+
+
+UV_VERSION := 0.11.2
+
+UV ?= $(shell command -v uv)
+UVX ?= $(shell command -v uvx)
+
+ifeq ($(strip $(UV)),)
+
+UV := $(BIN)/uv
+UVX := $(BIN)/uvx
+
+$(UV):
+	curl -L --output /tmp/uv.tar.gz https://github.com/astral-sh/uv/releases/download/$(UV_VERSION)/uv-$(ARCH)-unknown-linux-gnu.tar.gz
+	tar -xf /tmp/uv.tar.gz -C /tmp
+	rm /tmp/uv.tar.gz
+
+	test -d $(BIN) || mkdir -p $(BIN)
+
+	mv /tmp/uv-$(ARCH)-unknown-linux-gnu/uv $@
+	mv /tmp/uv-$(ARCH)-unknown-linux-gnu/uvx $(UVX)
+
+	$@ --version
+	$(UVX) --version
+
+endif
